a2947b7be8a5da3578978f0fdcb5da1c33376409
Dhruv Ghodadra <164461706+GhodadraDhruv@users.noreply.github.com>
2024-09-25 16:00:02 +0530

    Update Script_9.php

--- Task 1/Script_9.php
+++ Task 1/Script_9.php
@@ -2,6 +2,7 @@
      $name = "Dhruv";
      $age = 21;
      $city = "Rajkot";
+     $dob="14th march 2004";
  
-     echo $name . "<br>" . $age . "<br>" . $city;
+     echo $name . "<br>" . $age . "<br>" . $city . "<br>" . $dob;
 ?>
