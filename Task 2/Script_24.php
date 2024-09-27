bb75b4babce7de4ff4705f8ab7caf37b01f5ffeb
Dhruv Ghodadra <164461706+GhodadraDhruv@users.noreply.github.com>
2024-09-27 20:47:12 +0530

    Update Script_24.php

--- Task 2/Script_24.php
+++ Task 2/Script_24.php
@@ -1,8 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <label for="number">Enter a number:</label>
+        <input type="text" id="number" name="number"><br>
+        <input type="submit" value="submit" name="submit">
+    </form>
+</body>
+</html>
 <?php
+    if(isset($_GET['submit'])){
+        $number = $_GET['number'];
 
-$number = 18;
-$squareRoot = sqrt($number);
-
-echo "Square root of $number is: " . $squareRoot;
-
+        echo "Sqare root of $number: ". sqrt($number);
+    }
 ?>
