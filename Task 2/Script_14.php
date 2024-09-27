a0c7b0ab89e76d7422d0952b16f6b00f493e232f
Dhruv Ghodadra <164461706+GhodadraDhruv@users.noreply.github.com>
2024-09-27 20:32:45 +0530

    Update Script_14.php

--- Task 2/Script_14.php
+++ Task 2/Script_14.php
@@ -1,8 +1,10 @@
 <?php
 
-$sum = 0;
+    $sum = 0;
+    for($i=1;$i<=100;$i++){
+        $sum += $i;
+    }
 
-for ($i = 1; $i <= 100; $i++) {
-    $sum += $i;
-}
-echo "Sum Of 1 to 100 Is:", $sum;
+    echo $sum;
+
+?>
