6d53ed97e75410df0859b5b363255fdb8b23e0f3
Dhruv Ghodadra <164461706+GhodadraDhruv@users.noreply.github.com>
2024-09-27 20:42:26 +0530

    Update Script_21.php

--- Task 2/Script_21.php
+++ Task 2/Script_21.php
@@ -1,10 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <label for="number">Enter a number:</label>
+        <input type="text" id="number" name="number"><br>
+        <input type="submit" value="submit" name="submit">
+    </form>
+</body>
+</html>
 <?php
-$number = 18781;
-$numberStr = (string)$number;
-$reversedStr = strrev($numberStr);
-if ($numberStr === $reversedStr) {
-    echo $number . " is a palindrome.";
-} else {
-    echo $number . " is not a palindrome.";
-}
+    
+    if(isset($_GET['submit'])){
+        $number = $_GET['number'];
+
+        $no = $number; 
+        $reverse = 0;
+
+        if($number == 1){
+            $reverse = 1;
+        }
+
+        while($no>1){
+            $r = $no % 10;
+            $reverse = ($reverse * 10) +  $r;
+            $no = $no / 10;
+        }
+
+        if($number == $reverse){
+            echo "<br>$number is a palindrome number.";
+        }else{
+            echo "<br>$number is not a palindrome number.";
+        }
+    }
 ?>
