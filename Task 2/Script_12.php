a695211978c3f8a956da1656203829855e50b305
Dhruv Ghodadra <164461706+GhodadraDhruv@users.noreply.github.com>
2024-09-27 20:31:10 +0530

    Update Script_12.php

--- Task 2/Script_12.php
+++ Task 2/Script_12.php
@@ -1,13 +1,6 @@
 <?php
+    $name = "Dhruv";
+    $lastname = "Ghodadra";
 
-function concatenateString($string1, $string2)
-{
-    return $string1 . $string2;
-}
-
-
-$string1 = "Dhruv ,";
-$string2 = "Ghodadra";
-
-$combinedString = concatenateString($string1, $string2);
-echo "The combined string is: " . $combinedString;
+    echo $name . ' ' . $lastname;
+?>
