6db31ce78e6464dc4821e76bd17bc183ddd8bc7a
Dhruv Ghodadra <164461706+GhodadraDhruv@users.noreply.github.com>
2024-09-27 20:43:22 +0530

    Update Script_22.php

--- Task 2/Script_22.php
+++ Task 2/Script_22.php
@@ -1,8 +1,26 @@
 <?php
+    define("PI",3.14);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <label for="number">Enter a radius of circle:</label>
+        <input type="text" id="radius" name="radius"><br>
+        <input type="submit" value="submit" name="submit">
+    </form>
+</body>
+</html>
+<?php
+    if(isset($_GET['submit'])){
+        $radius = $_GET['radius'];
+        $area = PI * $radius * $radius;
 
-$radius = 18;
-
-$area = M_PI * pow($radius, 2);
-
-echo "The area of the circle with radius $radius is: $area";
+        echo "<br>Area of circle with radius($radius) : $area";
+    }
 ?>
